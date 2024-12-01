57ee1b0a9acbe5057304785e8f3ab792a791c09c
likhith <likhithuppala123@gmail.com>
2024-12-01 17:49:48 +0530

    commiting to github

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello, this is the portfolio</h1>
+        </div>
+    );
+};
+
+export default App;
